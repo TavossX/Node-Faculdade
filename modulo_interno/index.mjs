3d92a739af200c7764107af9153c975c40c1e1dd
OTAVIO MARTINS FICHO <0201392321024>
2025-02-19 08:00:30 -0300

    Deploy

---
+++ modulo_interno/index.mjs
@@ -0,0 +1,8 @@
+//import meu_modulo from "./meu_modulo.mjs";
+
+//console.log(meu_modulo(5, 5));
+
+//import pelo export nomeado
+
+import { soma } from "./meu_modulo.mjs";
+console.log(soma(5, 5));
