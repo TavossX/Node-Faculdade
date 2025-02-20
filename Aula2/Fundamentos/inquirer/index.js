cfdbfa3c8ce41b5f8bba9cebcdf00e9ce753d170
Otavio Martins <otaviomficho@gmail.com>
2025-02-20 09:27:56 -0300

    20-02

---
+++ Aula2/Fundamentos/inquirer/index.js
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+inquirer
+  .prompt([
+    {
+      name: "P1",
+      message: "Digite a nota da P1",
+      validate: (input) =>
+        isNaN(input) ? "Por favor digite um número valido" : true,
+    },
+    {
+      name: "P2",
+      message: "Digite a nota da P2",
+      validate: (input) =>
+        isNaN(input) ? "Por favor digite um número valido" : true,
+    },
+  ])
+  .then((answer) => {
+    console.log(answer);
+    console.log(`A nota da P1 é ${answer.P1}`);
+    console.log(`A nota da P2 é ${answer.P2}`);
+    const media = (parseFloat(answer.P1) + parseFloat(answer.P2)) / 2;
+    console.log(`A média é ${media}`);
+    if (media >= 6) {
+      console.log("Aprovado");
+    } else {
+      console.log("Reprovado");
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
